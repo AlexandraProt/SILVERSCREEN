7c4892b3b90882f93478d26818541b8ad20d876d
Alexandra Protsenko <sandraprotsenkos@gmail.com>
2025-06-12 17:57:45 +0000

    3

--- models/int/int_movies_costs.sql
+++ models/int/int_movies_costs.sql
@@ -1,10 +1,13 @@
-select month,
-Location_id, m-movie_id, movie_title, m.studio, genre,
-sum(total_invoice_sum) as rental_costs
-Arom 11 source ("sIlverscreen", "invoices") I as 1
-join f ref('stg_movie_catalogue') }} as m
-on
-i.movie_id=m.movie_id
-group by 1,2,3,4,5,6
-order by month
-desc
+select
+  month,
+  location_id,
+  m.movie_id,
+  movie_title,
+  m.studio,
+  genre,
+  sum(total_invoice_sum) as rental_costs
+from {{ source("silverscreen", "invoices") }} as i
+join {{ ref('stg_movie_catalogue') }} as m
+  on i.movie_id = m.movie_id
+group by 1, 2, 3, 4, 5, 6
+order by month desc
