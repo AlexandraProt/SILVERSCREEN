7d1af02a8b91fc9e50e6d9cc91b51669afae056c
Alexandra Protsenko <sandraprotsenkos@gmail.com>
2025-06-13 08:01:56 +0000

    final

--- models/silver/stg_nj_003.sql
+++ models/silver/stg_nj_003.sql
@@ -1,10 +1,8 @@
 select
-  coalesce(details, '2024_20C_014_130') as movie_id,
-  upper(coalesce(details, '2024_20C_014_130')) as location,
-  to_char(timestamp, 'YYYY-MM') as month,       
-  total_value as revenue,
-  amount as ticket_amount
-from {{ source('silverscreen', 'nj_003') }}
-where lower(product_type) = 'ticket'
-  and details is not null
-  and timestamp is not null
+    coalesce(details, '2024_20C_014_130') as movie_id,
+    upper(coalesce(details, '2024_20C_014_130')) as location,
+    to_char(timestamp, 'YYYY-MM') as month,
+    total_value as revenue,
+    amount as ticket_amount
+from {{ source("silverscreen", "nj_003") }}
+where lower(product_type) = 'ticket' and details is not null and timestamp is not null
